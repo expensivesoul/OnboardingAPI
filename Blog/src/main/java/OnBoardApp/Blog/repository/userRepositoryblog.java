ac1bfe5335c36c6a6e08abca6f646280f722f98b
nishantnineleaps <nishant.venugopal@nineleaps.com>
2019-08-05 11:54:44 +0530

    integrated version

--- Blog/src/main/java/OnBoardApp/Blog/repository/userRepositoryblog.java
+++
@@ -1,7 +0,0 @@
-package OnBoardApp.Blog.repository;
-
-import OnBoardApp.Blog.model.user;
-import org.springframework.data.repository.CrudRepository;
-
-public interface userRepository extends CrudRepository<user,Long> {
-}
