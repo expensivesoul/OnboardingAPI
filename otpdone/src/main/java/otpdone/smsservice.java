fb50c4b9eafb4217df2e8e25a5c0a08eeb7f7228
expensivesoul <akshay.gade@nineleaps.com>
2019-08-09 16:58:45 +0530

    funtioingopt

---
+++ otpdone/src/main/java/otpdone/smsservice.java
@@ -0,0 +1,40 @@
+package otpdone;
+
+
+
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class smsservice {
+
+	private final static String ACCOUNT_SID = "AC50141e72d3e82f3f867b152ba6b9739d";
+	private final static String AUTH_ID = "e5b6c0ce1495c9cef4064020939cc209";
+	
+	static {
+		Twilio.init(ACCOUNT_SID, AUTH_ID);
+	}
+//	
+//	public boolean send2FaCode(String mobilenumber, String twoFaCode) {
+//		
+//		Message.creator(new PhoneNumber(mobilenumber), new PhoneNumber("<TWILIO FROM NUMBER>"),
+//				"Your Two Factor Authentication code is: "+ twoFaCode).create();
+//		
+//		return true;
+//		
+//		
+//	}
+
+	public boolean send2FaCode(String mobile, String twoFaCode) {
+
+		Message.creator(new PhoneNumber(mobile), new PhoneNumber("+17244715038"),
+				"Hi"+mobile+"Your Two Factor Authentication code is: "+ twoFaCode).create();
+		
+		return true;
+		
+	}
+
+}
---
+++ otpdone/src/main/java/otpdone/user.java
@@ -0,0 +1,22 @@
+package otpdone;
+
+	
+
+import javax.persistence.Entity;
+
+@Entity
+public class user {
+	private String phone;
+
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+	
+
+
+}
